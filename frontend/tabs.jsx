e0a0e78488ed973d04a5311312dfe287cf8bf62c
brandontran <brandt2@uci.edu>
2019-02-15 16:56:46 -0800

    finish tabs widget

--- frontend/tabs.jsx
+++ frontend/tabs.jsx
@@ -3,20 +3,28 @@ import React from 'react';
 class Tabs extends React.Component {
   constructor(props){
     super(props);
-    this.state = {tabsIndex: 0};
+    this.state = {tabsIndex: 1};
+    this.changeTab = this.changeTab.bind(this);
   }
 
-  changeTab(e){
-    this.setState({ tabsIndex: e.target });
+  changeTab(newTabIndex){
+    this.setState({ tabsIndex: newTabIndex });
   }
 
   render(){
+    const thisTab = this.props.tabInfo[this.state.tabsIndex];
     return (
-      <ul>
-        <li><h1>{tabInfo[this.tabsIndex].title}</h1>
-          <header onClick={this.changeTab()}></header>
-          <article>{tabInfo[this.tabsIndex].content}</article>
+      <ul className="tabs-header">
+        <li className="tab">
+          <header onClick={() => this.changeTab(0)}><h1>{this.props.tabInfo[0].title}</h1></header>
         </li>
+        <li className="tab">
+          <header onClick={() => this.changeTab(1)}><h1>{this.props.tabInfo[1].title}</h1></header>
+        </li>
+        <li className="tab">
+          <header onClick={() => this.changeTab(2)}><h1>{this.props.tabInfo[2].title}</h1></header>
+        </li>
+          <article className="tab-content">{thisTab.content}</article>
       </ul>
     )
   }
--- frontend/widget.jsx
+++ frontend/widget.jsx
@@ -4,9 +4,9 @@ import Clock from './clock';
 import Tabs from './tabs';
 
 const tabInfo = [
-  { title: 'one', content: 'I am the first' },
-  { title: 'two', content: 'Second pane here' },
-  { title: 'three', content: 'Third pane here' }
+  { title: 'one', content: 'I am the first', index: 0 },
+  { title: 'two', content: 'Second pane here', index: 1 },
+  { title: 'three', content: 'Third pane here', index: 2 }
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
